bb476375db0d288e9b8b953f97a36c96d8b2eeb1
Monica1963 <amonica1963@gmail.com>
2021-04-07 00:56:56 -0300

    Header TodoList

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const express = require("express");
 const app = express();
 const logger = require("morgan");
 const compression = require("compression");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
+const path = require("path");
 
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 app.use(compression());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(logger("dev"));
 
+app.set("port", process.env.PORT || 3001);
+
+app.use(express.static(path.join(__dirname, "../frontend/public")));
+
 app.get("/", (req, res) => {
-  console.log("Hi desde el endpoint");
-  res.sendStatus(200);
+  res.sendFile(path.join(__dirname, "..", "frontend", "public", "index.html"));
 });
 
 // app.use(express.static("./public"));
 
-app.listen(3000);
+app.listen(app.get("port"), function () {
+  console.log(`Servidor iniciado en ${app.get("port")}`);
+});
